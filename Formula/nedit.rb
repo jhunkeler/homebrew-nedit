6abecec832b65be063cdab4fdcd5bafb91aa188b
Joseph Hunkeler <jhunkeler@gmail.com>
2024-01-29 09:55:22 -0500

    Apply patches

--- Formula/nedit.rb
+++ Formula/nedit.rb
@@ -2,14 +2,9 @@ class Nedit < Formula
   desc "Fast, compact Motif/X11 plain text editor w/ Debian patches"
   homepage "https://sourceforge.net/projects/nedit/"
   url "http://deb.debian.org/debian/pool/main/n/nedit/nedit_5.7.orig.tar.gz"
-  sha256 "32bf3fc15dbb6154838a34d560ca5b0f3e7744301f4c15f8a2af8ad2ea3d0764"
+  sha256 "add9ac79ff973528ad36c86858238bac4f59896c27dbf285cbe6a4d425fca17a"
   license "GPL-2.0-or-later"
-  revision 1
-
-  resource "patches" do
-    url "http://deb.debian.org/debian/pool/main/n/nedit/nedit_5.7-5.debian.tar.xz"
-    sha256 "32bf3fc15dbb6154838a34d560ca5b0f3e7744301f4c15f8a2af8ad2ea3d0764"
-  end
+  revision 1000
 
   depends_on "libice"
   depends_on "libsm"
@@ -22,11 +17,20 @@ class Nedit < Formula
 
   uses_from_macos "bison" => :build
 
+  patch do
+    url "https://deb.debian.org/debian/pool/main/n/nedit/nedit_5.7-5.debian.tar.xz"
+    sha256 "32bf3fc15dbb6154838a34d560ca5b0f3e7744301f4c15f8a2af8ad2ea3d0764"
+    apply %w[
+      patches/50_ChangeNCinMan.patch
+      patches/fix_typo_in_help.patch
+      patches/language-mode-detection.patch
+      patches/multiple-tabrows.patch
+      patches/show-filename.patch
+      patches/drag-move-v1_5.patch
+      patches/allow_reproducible_builds.patch
+    ]
+  end
   def install
-    resources("patches").stage { "." }
-    File.foreach("debian/patches/series", "\n") do |fp|
-      system "patch", "-p1", fp.chomp
-    end
     os = OS.mac? ? "macosx" : OS.kernel_name.downcase
     system "make", os, "MOTIFLINK='-lXm'"
     system "make", "-C", "doc", "man", "doc"
